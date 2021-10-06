54b70d4489d29a5a2ef3312ff32b3ead075c4bc4
pedro-candido <pedroferreirac@hotmail.com>
2021-10-06 05:52:05 -0300

    refactor(selectors): selectTask modified and comments removed

--- src/redux/reducers/tasks/selectors.ts
+++ src/redux/reducers/tasks/selectors.ts
@@ -1,7 +1,6 @@
-// import { useSelector } from "react-redux";
-// import State from "redux/store/IStore";
-// import { TasksProps } from "./types";
+import { useSelector } from "react-redux";
+import State from "redux/store/IStore";
+import { TasksProps } from "./types";
 
-/* export const selectTask = (): TasksProps =>
-  useSelector((state: State) => state.tasks);
-*/
+export const selectTask = (): TasksProps[] =>
+  useSelector(({ tasks }: State) => tasks);
